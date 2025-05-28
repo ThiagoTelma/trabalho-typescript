eb95d3caee2ea134963a7baf30c4747a4cd8b656
Thiago Telma <thiago04felipe@gmail.com>
2025-05-28 17:33:36 -0300

    Hotfix validar-senha

--- src/components/validar-senha.ts
+++ src/components/validar-senha.ts
@@ -2,29 +2,38 @@ import { esperarEnter, perguntar } from "../services/readline";
 
 async function validarSenha() {
     console.log("=== EXERCÍCIO 16 ===\n");
+    let senhaValida = false;
 
-    const senha = await perguntar("Digite uma senha: ");
+    do {
+        const senha = await perguntar("Digite uma senha: \n");
 
-    // Verifica se a senha tem pelo menos 8 caracteres
-    const tamanhoValido = senha.length >= 8;
+        // Verifica se a senha tem pelo menos 8 caracteres
+        const tamanhoValido = senha.length >= 8;
 
-    // Verifica se a senha tem pelo menos uma letra maiúscula
-    const contemMaiuscula = /[A-Z]/.test(senha);
+        // Verifica se a senha tem pelo menos uma letra maiúscula
+        const contemMaiuscula = /[A-Z]/.test(senha);
 
-    // Verifica se a senha tem pelo menos uma letra minúscula
-    const contemMinuscula = /[a-z]/.test(senha);
+        // Verifica se a senha tem pelo menos uma letra minúscula
+        const contemMinuscula = /[a-z]/.test(senha);
 
-    // Verifica se a senha tem pelo menos um número
-    const contemNumero = /\d/.test(senha);
+        // Verifica se a senha tem pelo menos um número
+        const contemNumero = /\d/.test(senha);
 
-    // A senha é válida se todos os critérios forem atendidos
-    if (tamanhoValido && contemMaiuscula && contemMinuscula && contemNumero) {
-        console.log("Senha válida!");
-    } else {
-        console.log(
-            "Senha inválida. Certifique-se de atender a todos os critérios."
-        );
-    }
+        // A senha é válida se todos os critérios forem atendidos
+        if (
+            tamanhoValido &&
+            contemMaiuscula &&
+            contemMinuscula &&
+            contemNumero
+        ) {
+            console.log("Senha válida! \n");
+            senhaValida = true;
+        } else {
+            console.log(
+                "Senha inválida. Certifique-se de atender a todos os critérios. \n"
+            );
+        }
+    } while (!senhaValida);
 
     await esperarEnter();
 }

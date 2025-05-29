e0d6b41944a9d5b69f981391d27d59010d01cf08
Felipe Leite <frleite64@gmail.com>
2025-05-29 19:54:26 -0300

    Hotfix validar-senha

--- src/components/validar-senha.ts
+++ src/components/validar-senha.ts
@@ -1,7 +1,8 @@
 import { esperarEnter, perguntar } from "../services/readline";
 
 async function validarSenha() {
-    console.log("=== EXERCÍCIO 16 ===\n");
+    console.log("=== EXERCÍCIO 16 ===");
+    console.log("A senha deve conter: no mínimo 8 caracteres, UMA letra maiúscula, UMA letra minúscula e pelo menos 1 número. \n");
     let senhaValida = false;
 
     do {
